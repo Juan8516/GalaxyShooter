e08d31c321faeb4ab222e0d0a75b28c0b0a2fce8
Juan8516 <juan.ind85@gmail.com>
2024-07-23 18:38:02 -0500

    feat: Implement lateral and vertical player movement
    - Added support for lateral movement (left and right) of the player.
    - Added vertical movement (up and down) of the player.
    - Updated 'Player.cs' script with movement logic.
    - Configured input controls to handle player movements.
    - Adjusted the stage boundaries to prevent the player from leaving the screen.
    - Tested and adjusted movement speeds for a smooth game experience.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //Variables
+    [SerializeField]
+    private float speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      transform.position = new Vector3(0, -4, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    private void Movement()
+    {
+        //Movement player
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * verticalInput * Time.deltaTime);
+
+        //Limits player in Y
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < -4.5)
+        {
+            transform.position = new Vector3(transform.position.x, -4.5f, 0);
+        }
+
+        //Limits player in X
+        if (transform.position.x > 9.4)
+        {
+            transform.position = new Vector3(-9.4f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -9.4)
+        {
+            transform.position = new Vector3(9.4f, transform.position.y, 0);
+        }
     }
 }
